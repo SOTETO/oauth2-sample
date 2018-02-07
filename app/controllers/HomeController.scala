634a0469cf8f505bfb549a7acd8b8a2c3a5e56ff
Johann Sell <sell@informatik.hu-berlin.de>
2018-02-07 14:17:35 +0100

    reactivate secrets on ws communication (not on redirect)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -37,14 +37,14 @@ class HomeController @Inject() (ws: WSClient,conf : Configuration) extends Contr
   def receiveCode(code: String) = Action.async {
     val url = conf.getString("drops.url.base").get + conf.getString("drops.url.accessToken").get
     val clientId = conf.getString("drops.client_id").get
-//    val clientSecret = conf.getString("drops.client_secret").get
+    val clientSecret = conf.getString("drops.client_secret").get
 
     val accessToken = ws.url(url).withQueryString(
       "grant_type" -> "authorization_code",
       "client_id" -> clientId,
-//      "client_secret" -> clientSecret,
+      "client_secret" -> clientSecret,
       "code" -> code,
-      "redirect_uri" -> "http://localhost:8000/endpoint?code="
+      "redirect_uri" -> "http://localhost:8080/endpoint?code="
     ).get().map(response => response.status match {
       case 200 => AccessToken(response.json)
       case _ => println(response.status);throw new Exception // Todo: throw meaningful exception considering the returned error message and status code!
